759e16c4b7b01381a1434b3b477385feb306512b
jmechristian <jmechristian@gmail.com>
2026-02-17 13:01:54 -0500

    ssss

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -8,9 +8,9 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 import { getOrCreateAWSUser } from "./awsUser";
 
-const appBaseUrl =
-  process.env.APP_BASE_URL ||
-  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3001");
+const appBaseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : (process.env.APP_BASE_URL || "http://localhost:3001");
 
 export const auth0 = new Auth0Client({
   appBaseUrl,
